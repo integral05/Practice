c664968acd0c6f334210637ab8deac9aa73fe639
integral05 <tejastathe4337@gmail.com>
2024-01-15 11:36:29 +0530

    Added program2.py file

---
+++ program2.py
@@ -0,0 +1,19 @@
+a=int(input("Enter your value for a:"))
+b=int(input("Enter your value for b:"))
+
+#logical operators
+print(a and b)  #if value of a is 0 o/p is 0 & if value of a is non zero it will print value of b
+print(a or b)
+print( not a)
+print( not b)
+
+
+#assignment operators
+x= int(input("Enter your value for x:"))
+y=int(input("Enter your value for y:"))
+#x=10
+#y=5
+x+=5
+y-=2
+print(x)
+print(y)
